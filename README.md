7e31ccf2ac384476578b5c9d75c4fd5d4a181e6b
cocktailpeanut <cocktailpeanuts@proton.me>
2023-07-14 11:09:56 -0400

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# companion-app.pinokio
+
+A one-click installer for https://github.com/a16z-infra/companion-app powered by [Pinokio](https://pinokio.computer/)
+
---
+++ pinokio.js
@@ -0,0 +1,9 @@
+module.exports = {
+  menu: [{
+    "html": "<i class='fa-solid fa-gear'></i> Install",
+    "href": "install.json"
+  }, {
+    "html": "<i class='fa-solid fa-play'></i> Launch",
+    "href": "start.json"
+  }]
+}
